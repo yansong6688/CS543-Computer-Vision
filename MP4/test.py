dce3018119dd447e0a87bbbe701f847b5c784a7f
jasonjnie <jasonniejx@gmail.com>
2018-04-30 22:23:32 -0500

    updated

--- MP4/test.py
+++ MP4/test.py
@@ -5,6 +5,7 @@ import hashlib
 
 def check_integrity(fpath, md5):
     if not os.path.isfile(fpath):
+        print('1')
         return False
     md5o = hashlib.md5()
     with open(fpath, 'rb') as f:
@@ -13,12 +14,13 @@ def check_integrity(fpath, md5):
             md5o.update(chunk)
     md5c = md5o.hexdigest()
     if md5c != md5:
+        print('2')
         return False
     return True
 
 
 PATH_TO_CIFAR100_CS543 = "./"
-FILENAME = "cifar-100-cs543-python.tar.gz"
+FILENAME = "cifar-100-cs543-python.tar"
 tgz_md5 = 'e68a4c763591787a0b39fe2209371f32'
 fpath = os.path.join(PATH_TO_CIFAR100_CS543, FILENAME)
 print('fpath =', fpath)
